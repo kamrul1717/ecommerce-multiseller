c93b4662aa5fd3049a80bf154bdf3c83c84a793d
unknown <kamrulhassan1361@gmail.com>
2020-02-08 20:56:09 +0600

    initiated the project and worked with product page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container text-center">
+    <h2>Products</h2>
+
+        <div class="row">
+
+            @foreach($allProducts as $product)
+
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('cat.jpg') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $product->name }}</h4>
+                        <p class="card-text">{{ $product->description }}</p>
+                    </div>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+
+            @endforeach
+
+        </div>
+
+</div>
+@endsection
